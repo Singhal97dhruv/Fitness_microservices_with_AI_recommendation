0f7f4fd998d736faa2a34c01bb2ebcbea60c9c00
Singhal <dhruv.b.singhal@accenture.com>
2025-10-08 10:57:35 +0530

    corrected ActivityServiceInterface

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -5,11 +5,6 @@ import com.fitness.activityservice.dto.ActivityResponse;
 
 public interface ActivityService {
 
-<<<<<<< HEAD
-    ActivityResponse trackActivity(ActivityRequest activityRequest);
-    Boolean validateUser(String userId);
-=======
     public ActivityResponse trackActivity(ActivityRequest activityRequest);
     public Boolean validateUser(String userId);
->>>>>>> 1a6cf9ec19300989482b8d1458c97ea0f065a07f
 }
